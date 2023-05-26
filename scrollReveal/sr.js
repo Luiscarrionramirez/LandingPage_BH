768f54712f6c4a0dce24ebd8bd1c631c95654e55
Luiscarrionramirez <127272708+Luiscarrionramirez@users.noreply.github.com>
2023-05-25 23:13:42 -0400

    Add files via upload

---
+++ scrollReveal/sr.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+   sr.reveal('.header',{
+      duration: 2000,
+      origin: 'top',
+      distance: '100px'
+   });
+
+window.sr = ScrollReveal();
+   sr.reveal('.img-container',{
+      duration: 2100,
+      origin: 'top',
+      distance: '100px'
+   });
+
+   window.sr = ScrollReveal();
+   sr.reveal('.services',{
+      duration: 2000,
+      origin: 'bottom',
+      distance: '150px'
+   });
+
+
+   window.sr = ScrollReveal();
+   sr.reveal('.logos',{
+      duration: 1800
+   });
+
+   window.sr = ScrollReveal();
+   sr.reveal('.end',{
+      duration: 2000,
+      origin: 'right',
+      distance: '180px'
+   });
